34ebaf85e95c08b536ead5d33a1f683f39583a83
Caio Furtado <caiohbfurtado@gmail.com>
2021-10-20 17:28:55 -0300

    create functions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,36 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const existsTask = tasks.some(currentTask => currentTask.id === id);
+
+    if (!existsTask) {
+      return;
+    }
+
+    setTasks(prevState => prevState.map(currentTask => currentTask.id === id ? {
+      ...currentTask,
+      done: !currentTask.done,
+    } : {...currentTask}));
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const existsTask = tasks.some(currentTask => currentTask.id === id);
+
+    if (!existsTask) {
+      return;
+    }
+    
+    setTasks(prevState => prevState.filter(currentTask => currentTask.id !== id));
   }
 
   return (
